57d3dc27b60a345d3d100aaf43099de77cdf651f
Laura <lauracolof@gmail.com>
2023-06-01 23:12:17 -0300

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'welcome', to: 'home#index'
   root to: 'home#index'
-
-  get "articles/user/:user_id", to: "articles#from_author"
-
   # la ruta /article/... no funcionara si no creamos primero la ruta /article, dará ERROR: Missing :controller key on routes definition, please... porque 
-  get 'articles', to:'articles#index'
-  post 'articles', to: 'articles#create'
-  get 'articles/new', to: 'articles#new', as: :new_articles
-  get 'articles/:id', to: 'articles#show'
-  get 'articles/:id/edit', to: 'articles#edit'
-  # patch '/articles/:id', to: 'articles#update', as: article
-  post 'articles', to: 'articles#create'
-  delete '/articles/:id', to:  'articles#destroy'
+
+  resources :articles do
+    get "user/:user_id", to: "articles#from_author", on: :collection
+  end
+  #todas las lineas siguientes, se generan con resources.
+
+  # get 'articles', to:'articles#index'
+  # post 'articles', to: 'articles#create'
+  # get 'articles/new', to: 'articles#new', as: :new_articles
+  # get 'articles/:id', to: 'articles#show'
+  # get 'articles/:id/edit', to: 'articles#edit'
+  # # patch '/articles/:id', to: 'articles#update', as: article
+  # post 'articles', to: 'articles#create'
+  # delete '/articles/:id', to:  'articles#destroy'
 end
-
-
-# RAILS ROUTES NOS DEVUELVE TODOS LOS NOMBRES DE LAS RUTAS Y SUS MÉTODOS, y se coloca ese nombre_path para agregarlas en navbar por ej
